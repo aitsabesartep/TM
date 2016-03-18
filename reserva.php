008818af45930130ccb1df6e8e5218e312a1410c
aitsabesartep <ssb@ssb-2.local>
2016-03-18 17:55:49 +0100

    xml

--- reserva.php
+++ reserva.php
@@ -1,22 +1,53 @@
 <?php
 
-	$file = simplexml_load_file("fincas.xml");
+// Load XML file 
+$doc = new DOMDocument(); 
+$doc->preserveWhiteSpace = false; 
+$doc->load( "fincas.xml" ); 
+$doc->formatOutput = true; 
 
-	$finca = $file->finca[0];//Fer condicio
+// Get the stocklist root element 
+$stockListElements = $doc->getElementsByTagName(  "calendario" ); 
+$stockList = $stockListElements->item( 0 ); 
 
-	$calendari = $finca->calendario;
 
-	//$registro = $file->createElement('test', 'This is the root element!');
 
-	$registre = new SimpleXMLElement('<registre></registre>');
+// Create a new "item" element and add it to the stocklist 
+$item = $doc->createElement( "registre"); 
+$stockList->appendChild( $item );
 
-	$registre->addChild('dia', '8');
-	$registre->addChild('mes', '7');
-	$registre->addChild('any', '2016');
-	$registre->addChild('ndias', '3');
+$stockListElements = $doc->getElementsByTagName(  "registre" ); 
+$stockList = $stockListElements->item( 0 );
 
-	$calendari->addChild('<calendari></calendari>',$registre);
+$item = $doc->createElement( "dia", "7"); 
+$stockList->appendChild( $item ); 
+$item = $doc->createElement( "mes", "1"); 
+$stockList->appendChild( $item ); 
+$item = $doc->createElement( "any", "2016"); 
+$stockList->appendChild( $item );  
+$item = $doc->createElement( "numdies", "5"); 
+$stockList->appendChild( $item ); 
 
-	// Display the new XML code
-	$file->asXml('fincas.xml');
+/*
+// Create the item's "name" child element 
+$name = $doc->createElement( "name", "carrot" ); 
+$item->appendChild( $name ); 
+
+// Create the item's "unitprice " child element 
+$unitPrice = $doc->createElement( "unitPrice", "0.79" ); 
+$item->appendChild( $unitPrice ); 
+
+//Create item's quantity element 
+$quantity = $doc->createElement( "quantity", "31" ); 
+$item->appendChild( $quantity ); 
+
+// Create item's description element 
+$description = $doc->createElement( "description" ); 
+$item->appendChild( $description ); 
+$cdata = $doc->createCDATASection( "Carrots are crunchy" ); 
+$description->appendChild( $cdata ); 
+*/
+//Output the XML document, encoding markup characters as needed 
+echo htmlspecialchars( $doc->saveXML() );
+echo htmlspecialchars( $doc->save('fincas.xml') );
 ?>
